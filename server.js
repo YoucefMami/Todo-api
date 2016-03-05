17f849ae23401ae1002909a9edbf47504e9dbf2c
Youcef Mami <youcef.mami@gmail.com>
2016-03-05 16:41:21 -0500

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -2,9 +2,46 @@ var express = require('express');
 var app = express();
 // process.env.PORT is an ENV variable provided by Heroku
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Clean apartment',
+	completed: true
+}];
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10); //req.params.id is a string
+	var matchedTodo;
+
+	for (var i = 0; i < todos.length; i++) {
+		if (todos[i].id === todoId) {
+			matchedTodo = todos[i];
+			break;
+		}
+	}
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send('Todo with id of ' + req.params.id + ' does not exist!');	
+	}
 });
 
 // Callback
