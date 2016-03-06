2efe3b78158132aeb32f5c3c0bc538f234c1e6da
Youcef Mami <youcef.mami@gmail.com>
2016-03-05 19:34:22 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 // process.env.PORT is an ENV variable provided by Heroku
@@ -23,14 +24,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10); //req.params.id is a string
-	var matchedTodo;
-
-	for (var i = 0; i < todos.length; i++) {
-		if (todos[i].id === todoId) {
-			matchedTodo = todos[i];
-			break;
-		}
-	}
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -41,8 +35,14 @@ app.get('/todos/:id', function (req, res) {
 
 // POST
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	// Filter user JSON to only have description and completed keys
+	var body = _.pick(req.body, ['description', 'completed']);
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send('Bad data was provided');
+	}
 	
+	body.description = body.description.trim();
 	body.id = todoNextId++; //incrementation after current value is assigned to body.id
 	
 	todos.push(body);
