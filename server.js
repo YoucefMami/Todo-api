4692cc78f658865e100d32ae7c7c5ce1edb190d8
Youcef Mami <youcef.mami@gmail.com>
2016-03-10 00:12:17 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 // process.env.PORT is an ENV variable provided by Heroku
@@ -59,16 +60,22 @@ app.post('/todos', function(req, res) {
 	// Filter user JSON to only have description and completed keys
 	var body = _.pick(req.body, ['description', 'completed']);
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send('Bad data was provided');
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	body.description = body.description.trim();
-	body.id = todoNextId++; //incrementation after current value is assigned to body.id
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send('Bad data was provided');
+	// }
 
-	todos.push(body);
+	// body.description = body.description.trim();
+	// body.id = todoNextId++; //incrementation after current value is assigned to body.id
 
-	res.json(body);
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -123,8 +130,10 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
+db.sequelize.sync().then(function () {
+	// Callback
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
+});
 
-// Callback
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
-})
