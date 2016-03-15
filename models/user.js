ffca6b7db34ca843819d20aa5fc3df72fbde23f7
Youcef Mami <youcef.mami@gmail.com>
2016-03-14 22:17:58 -0400

    Add user model and POST /users request

---
+++ models/user.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			// can impose special characters, check documentation
+			validate: {
+				len: [7, 100]
+			}
+		}
+	});
+};
--- server.js
+++ server.js
@@ -109,6 +109,18 @@ app.post('/todos', function(req, res) {
 	// res.json(body);
 });
 
+// POST
+app.post('/users', function(req, res) {
+	// Filter user JSON to only have email and password keys
+	var body = _.pick(req.body, ['email', 'password']);
+
+	db.user.create(body).then(function (user) {
+		res.json(user.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+});
+
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10); //req.params.id is a string
